d1be848ae07a5e5e45300c2b06a40e56efabec3c
Surekha390 <surekha.tallapalli@gmail.com>
2025-03-22 11:08:51 +0100

    Environment variables

---
+++ 11. variable-assignment/variables.tf
@@ -0,0 +1,14 @@
+variable "instance_type" {}
+
+/* Added environment variable
+Creating Environment variables:
+
+Run —> type sysdm.cpl  (it opens system properties) —>ok  —> Advanced —> Environment variables —> click on 1st new, under user variables section  
+—>give variable name and value there
+
+and click on OK.
+
+Ex: variable name:  TF_VAR_instance_type
+
+variable value: t2.micro
+*/
