edce8b600c5d74e5dca895ac662efdf682921296
Surekha390 <surekha.tallapalli@gmail.com>
2025-03-23 17:52:10 +0100

    fetch latest AMI and create EC2 instance with this AMI

---
+++ 25. Data Source - Latest AMI/ami-ec2.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+    region = "us-west-2"
+}
+
+data "aws_ami" "myimage" {
+  most_recent      = true
+  owners           = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+}
+
+resource "aws_instance" "my_ec2" {
+  ami = data.aws_ami.myimage.image_id
+  instance_type = "t2.micro"
+  subnet_id = "subnet-035810c6619ba14aa"
+}
